4dd939ee1533175bf96559eb2fd952713c32d3a0
lifegatejustice <lifegatedetom@gmail.com>
2025-09-17 08:50:07 +0100

    `Added CORS middleware and updated dependencies in package.json and package-lock.json`

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+const cors = require('cors');
 const swaggerJsdoc = require('swagger-jsdoc');
 const swaggerUi = require('swagger-ui-express');
 
@@ -15,6 +16,7 @@ const courseRoutes = require('./routes/courseRoutes');
 const app = express();
 
 // Middleware
+app.use(cors());
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true }));
 
@@ -31,6 +33,10 @@ const swaggerOptions = {
       {
         url: 'http://localhost:3000',
         description: 'Development server',
+      },
+      {
+        url: 'https://student-api-cdu6.onrender.com',
+        description: 'Render deployment server',
       },
     ],
   },
